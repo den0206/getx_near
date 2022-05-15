8b6317634a6bdb5028d4ba4f28744c9c39b1f1c1
yuuki sakai <s.yuuki0206@gmail.com>
2022-05-16 08:26:18 +0900

    location

--- near_client/lib/main.dart
+++ near_client/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get_instance/src/extension_instance.dart';
 import 'package:get/route_manager.dart';
@@ -17,6 +18,10 @@ void main() async {
   await dotenv.load(fileName: ".env");
 
   await Get.put(NotificationService()).initService();
+
+  FirebaseMessaging.onBackgroundMessage(
+      NotificationService.to.firebaseMessagingBackgroundHandler);
+
   runApp(DevicePreview(enabled: false, builder: (context) => MyApp()));
 }
 
