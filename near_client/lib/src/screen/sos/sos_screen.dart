776a3aadd302e51dc9d2492f9e60ef375280a0c9
yuuki sakai <s.yuuki0206@gmail.com>
2022-05-21 16:57:06 +0900

    init spind funcs

--- near_client/lib/src/screen/sos/sos_screen.dart
+++ near_client/lib/src/screen/sos/sos_screen.dart
@@ -1,38 +1,76 @@
-import 'package:flutter/material.dart';
+import 'package:flutter_neumorphic/flutter_neumorphic.dart';
+import 'package:get/get_state_manager/get_state_manager.dart';
+import 'package:getx_near/src/screen/sos/sos_controller.dart';
+import 'package:getx_near/src/screen/widget/blinking_widget.dart';
+import 'package:getx_near/src/screen/widget/neumorphic/longpress_button.dart';
 import 'package:getx_near/src/utils/consts_color.dart';
+import 'package:sizer/sizer.dart';
 
 class SOSScreen extends StatelessWidget {
   const SOSScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('SOS'),
-      ),
-      body: Center(
-          child: Container(
-        height: 50,
-        width: 150,
-        decoration: BoxDecoration(
-          color: ConstsColor.panelColor,
-          borderRadius: BorderRadius.circular(15),
-          boxShadow: [
-            BoxShadow(
-              color: Colors.purple,
-              spreadRadius: 1,
-              blurRadius: 8,
-              offset: Offset(4, 4),
+    return GetBuilder<SOSController>(
+      init: SOSController(),
+      builder: (controller) {
+        return Scaffold(
+          body: Center(
+            child: LongNeumorphicButton(
+              onPressed: () => controller.play(),
+              onEnded: () => controller.pause(),
+              minDistance: -5,
+              style: NeumorphicStyle(
+                shape: NeumorphicShape.concave,
+                boxShape:
+                    NeumorphicBoxShape.roundRect(BorderRadius.circular(12)),
+                depth: 10,
+                lightSource: LightSource.topLeft,
+                color: ConstsColor.panelColor,
+              ),
+              child: Container(
+                width: 70.w,
+                height: 66.h,
+                child: Center(
+                  child: Obx(
+                    () => BlinkingWidet(
+                      use: !controller.isPlaying.value,
+                      duration: Duration(seconds: 3),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          // Text(
+                          //   "SOS",
+                          //   style:
+                          //       TextStyle(color: Colors.red, fontSize: 30.sp),
+                          // ),
+                          Icon(
+                            Icons.local_police,
+                            color: Colors.yellow,
+                            size: 70,
+                          ),
+                          SizedBox(
+                            height: 5.h,
+                          ),
+                          Text(
+                            // "HELP",
+                            "Call The Police!",
+                            style: TextStyle(
+                                color: controller.isPlaying.value
+                                    ? Colors.red
+                                    : Colors.blue,
+                                fontSize: 20.sp),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ),
             ),
-            BoxShadow(
-              color: Colors.white,
-              spreadRadius: 2,
-              blurRadius: 8,
-              offset: Offset(-4, -4),
-            ),
-          ],
-        ),
-      )),
+          ),
+        );
+      },
     );
   }
 }
