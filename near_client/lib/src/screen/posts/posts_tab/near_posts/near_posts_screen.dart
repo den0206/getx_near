1f481ae7a6a827771483e8529e0f690ed08c9437
den0206 <s.yuuki0206@gmail.com>
2022-08-11 07:26:53 +0900

    embty area

--- near_client/lib/src/screen/posts/posts_tab/near_posts/near_posts_screen.dart
+++ near_client/lib/src/screen/posts/posts_tab/near_posts/near_posts_screen.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_neumorphic/flutter_neumorphic.dart';
 import 'package:get/get_state_manager/get_state_manager.dart';
+import 'package:getx_near/src/screen/widget/neumorphic/nicon_button.dart';
+import 'package:sizer/sizer.dart';
 import '../my_post_tab_screen.dart';
 import '../my_posts/my_posts_screen.dart';
 import 'near_posts_controller.dart';
@@ -42,6 +46,38 @@ class NearPostsScreen extends GetView<NearPostsController> {
                 childCount: controller.nearPosts.length,
               ),
             ),
+            if (controller.isCompEmpty) ...[
+              SliverFillRemaining(
+                hasScrollBody: false,
+                fillOverscroll: true,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    NeumorphicIconButton(
+                      depth: 1,
+                      icon: Icon(
+                        Icons.location_on,
+                        size: 120.sp,
+                        color: Colors.redAccent,
+                      ),
+                      color: Colors.yellow.withOpacity(0.3),
+                      onPressed: () async {
+                        await controller.getNearPosts();
+                      },
+                    ),
+                    SizedBox(
+                      height: 5.h,
+                    ),
+                    Text(
+                      "現在地から検索を行う",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                      ),
+                    )
+                  ],
+                ),
+              )
+            ]
           ],
         );
       },
