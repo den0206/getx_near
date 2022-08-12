0358bd4d1093d607b97be8470ed55e97a26647db
den0206 <s.yuuki0206@gmail.com>
2022-08-12 21:29:38 +0900

    fix report api

---
+++ near_client/lib/src/api/report_api.dart
@@ -0,0 +1,17 @@
+import 'package:getx_near/src/api/api_base.dart';
+import 'package:getx_near/src/model/utils/response_api.dart';
+
+class ReportAPI extends APIBase {
+  ReportAPI() : super(EndPoint.comment);
+
+  Future<ResponseAPI> sendReport(
+      {required Map<String, dynamic> reportData}) async {
+    try {
+      final Uri uri = setUri("/create");
+
+      return await postRequest(uri: uri, body: reportData, useToken: true);
+    } catch (e) {
+      throw e;
+    }
+  }
+}
