be71a666d72dadb79551031d4107c49b1ac9881f
den0206 <s.yuuki0206@gmail.com>
2022-09-04 15:17:31 +0900

    fix notification api

--- near_client/lib/src/api/notification_api.dart
+++ near_client/lib/src/api/notification_api.dart
@@ -1,22 +1,37 @@
-import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:getx_near/src/api/api_base.dart';
 import 'package:getx_near/src/model/utils/response_api.dart';
 
 class NotificationAPI extends APIBase {
-  final String? serverKey = dotenv.env["FCM_SERVER_KEY"];
-  NotificationAPI() : super(EndPoint.notification) {
-    this.host = "fcm.googleapis.com";
-  }
+  NotificationAPI() : super(EndPoint.notification);
 
   Future<ResponseAPI> sendNotification(Map<String, dynamic> body) async {
     try {
-      final Uri uri = Uri.https(host, "${endPoint.name}/send");
-      if (serverKey == null) throw new Exception("No Server Key");
-      headers["Authorization"] = "key=${serverKey}";
+      final Uri uri = setUri("/send");
 
-      return await postRequest(uri: uri, body: body);
+      return await postRequest(uri: uri, useToken: true, body: body);
+    } catch (e) {
+      return catchAPIError(e.toString());
+    }
+  }
+
+  Future<ResponseAPI> getBadgesCount({required String userId}) async {
+    final q = {"userId": userId};
+    try {
+      final Uri uri = setUri("/getBadgeCount", q);
+      return await getRequest(uri: uri, useToken: true);
     } catch (e) {
       return catchAPIError(e.toString());
     }
   }
 }
+
+
+
+  // final String? serverKey = dotenv.env["FCM_SERVER_KEY"];
+  // NotificationAPI() : super(EndPoint.notification) {
+  //   this.host = "fcm.googleapis.com";
+  // }
+
+  // final Uri uri = Uri.https(host, "${endPoint.name}/send");
+  // if (serverKey == null) throw new Exception("No Server Key");
+  // headers["Authorization"] = "key=${serverKey}";
