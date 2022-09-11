7dbb95b7c84116ee8b09251f745ea370fc3c1dfc
yuuki sakai <s.yuuki0206@gmail.com>
2022-09-11 14:59:45 +0900

    add tutorial

--- near_client/lib/src/screen/map/slide_panel/main_slide_panel_screen.dart
+++ near_client/lib/src/screen/map/slide_panel/main_slide_panel_screen.dart
@@ -3,6 +3,7 @@ import 'package:get/get_state_manager/get_state_manager.dart';
 import 'package:getx_near/src/screen/map/map_controller.dart';
 import 'package:getx_near/src/screen/map/map_service.dart';
 import 'package:getx_near/src/screen/map/slide_panel/main_slide_panel_controller.dart';
+import 'package:getx_near/src/screen/widget/Common_showcase.dart';
 import 'package:getx_near/src/screen/widget/custom_slider.dart';
 import 'package:getx_near/src/screen/widget/neumorphic/nicon_button.dart';
 import 'package:getx_near/src/screen/widget/origin_carousel.dart';
@@ -39,76 +40,80 @@ class MainSlideUpPanel extends GetView<MainSlidePanelController> {
           ),
           onPanelSlide: controller.mapController.mapService.changePanelPosition,
           panelBuilder: (sc) {
-            return Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                Container(
-                  width: 30,
-                  height: 5,
-                  decoration: BoxDecoration(
-                      color: Colors.black,
-                      borderRadius: BorderRadius.all(Radius.circular(12.0))),
-                ),
-                OriginCarousel(
-                  pageController: controller.pageController,
-                  itemCount: controller.mPosts.length,
-                  onChange: controller.postsOnChange,
-                  itemBuilder: (context, index) {
-                    final post = controller.mPosts[index];
-                    return OriginCarouselCell(
-                      currentIndex: controller.currentPostIndex,
-                      index: index,
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: [
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              Spacer(),
+            return CommonShowcaseWidget(
+              key: mapController.tutorialKey4,
+              description: "近隣の投稿が表示されます。",
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Container(
+                    width: 30,
+                    height: 5,
+                    decoration: BoxDecoration(
+                        color: Colors.black,
+                        borderRadius: BorderRadius.all(Radius.circular(12.0))),
+                  ),
+                  OriginCarousel(
+                    pageController: controller.pageController,
+                    itemCount: controller.mPosts.length,
+                    onChange: controller.postsOnChange,
+                    itemBuilder: (context, index) {
+                      final post = controller.mPosts[index];
+                      return OriginCarouselCell(
+                        currentIndex: controller.currentPostIndex,
+                        index: index,
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Spacer(),
+                                Text(
+                                  post.user.name,
+                                  maxLines: 1,
+                                  overflow: TextOverflow.ellipsis,
+                                  textAlign: TextAlign.center,
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 15.sp,
+                                  ),
+                                ),
+                                Spacer(),
+                                Padding(
+                                  padding: const EdgeInsets.only(right: 10),
+                                  child: HelpButton(
+                                    post: post,
+                                    size: 23.sp,
+                                  ),
+                                ),
+                              ],
+                            ),
+                            UserAvatarButton(
+                              user: post.user,
+                              size: 70.sp,
+                            ),
+                            AlertIndicator(
+                              intValue: post.emergency,
+                              level: post.level,
+                              height: 20,
+                            ),
+                            if (post.distance != null)
                               Text(
-                                post.user.name,
-                                maxLines: 1,
-                                overflow: TextOverflow.ellipsis,
-                                textAlign: TextAlign.center,
-                                style: TextStyle(
-                                  color: Colors.white,
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: 15.sp,
-                                ),
-                              ),
-                              Spacer(),
-                              Padding(
-                                padding: const EdgeInsets.only(right: 10),
-                                child: HelpButton(
-                                  post: post,
-                                  size: 23.sp,
-                                ),
-                              ),
-                            ],
-                          ),
-                          UserAvatarButton(
-                            user: post.user,
-                            size: 70.sp,
-                          ),
-                          AlertIndicator(
-                            intValue: post.emergency,
-                            level: post.level,
-                            height: 20,
-                          ),
-                          if (post.distance != null)
-                            Text(
-                              "約 ${distanceToString(post.distance!)} km",
-                              style: TextStyle(fontWeight: FontWeight.bold),
-                            )
-                        ],
-                      ),
-                      onTap: () {
-                        controller.selectPost(post);
-                      },
-                    );
-                  },
-                )
-              ],
+                                "約 ${distanceToString(post.distance!)} km",
+                                style: TextStyle(fontWeight: FontWeight.bold),
+                              )
+                          ],
+                        ),
+                        onTap: () {
+                          controller.selectPost(post);
+                        },
+                      );
+                    },
+                  )
+                ],
+              ),
             );
           },
         );
