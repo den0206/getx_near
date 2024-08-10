7700697b740003f88ada91e5aa7a21945eafb67c
den0206 <s.yuuki0206@gmail.com>
2024-08-10 10:19:54 +0900

    adjust 3.24.0

--- near_client/ios/Runner/AppDelegate.swift
+++ near_client/ios/Runner/AppDelegate.swift
@@ -4,7 +4,7 @@ import flutter_config
 import GoogleMaps
 
 
-@UIApplicationMain
+@main
 @objc class AppDelegate: FlutterAppDelegate {
   override func application(
     _ application: UIApplication,
