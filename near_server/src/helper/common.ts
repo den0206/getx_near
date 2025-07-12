0126c76f46aac9edd34ed0f82767883cc7318ab3
den0206 <s.yuuki0206@gmail.com>
2025-07-12 09:57:16 +0900

    monggose id error 修正

--- near_server/src/helper/common.ts
+++ near_server/src/helper/common.ts
@@ -8,18 +8,18 @@ export function commoneSchemaOption({
   return {
     schemaOptions: {
       toJSON: {
-        transform: function (_, ret) {
+        transform: function (_, ret: any) {
           // replace _id to id
-          // ret.id = ret._id;
-          // delete ret._id;
-          // delete ret.__v;
+          ret.id = ret._id;
+          // remove _id & __v
+          delete ret._id;
+          delete ret.__v;
         },
         versionKey: false,
       },
       // _vをつけない様にする
       versionKey: false,
       timestamps: useTimestamp || false,
-      id: true,
     },
   };
 }
