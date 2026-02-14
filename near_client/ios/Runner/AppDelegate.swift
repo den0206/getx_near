4efee6fdc2964c25da1b0a2d32920dc11552fe12
den0206 <s.yuuki0206@gmail.com>
2026-02-14 09:47:09 +0900

    Flutter 3.41.1 対応

--- near_client/ios/Runner/AppDelegate.swift
+++ near_client/ios/Runner/AppDelegate.swift
@@ -5,18 +5,17 @@ import GoogleMaps
 
 
 @main
-@objc class AppDelegate: FlutterAppDelegate {
-  override func application(
-    _ application: UIApplication,
-    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
-  ) -> Bool {
-
-
-
-    GMSServices.provideAPIKey(FlutterConfigPlugin.env(for:"GOOGLE_MAPS_IOS_KEY"))  
-
+@objc class AppDelegate: FlutterAppDelegate, FlutterImplicitEngineDelegate {
+    override func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+    ) -> Bool {
+        GMSServices.provideAPIKey(FlutterConfigPlugin.env(for:"GOOGLE_MAPS_IOS_KEY"))
+        return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+    }
     
-    GeneratedPluginRegistrant.register(with: self)
-    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-  }
+    // NEW: This is where plugin registration lives now
+    func didInitializeImplicitFlutterEngine(_ engineBridge: FlutterImplicitEngineBridge) {
+        GeneratedPluginRegistrant.register(with: engineBridge.pluginRegistry)
+    }
 }
